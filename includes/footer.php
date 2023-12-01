616d5b548cbdb5d474cb351c0c842e1a65d3cdeb
RAMUL <flyingbayblade@gmail.com>
2023-12-02 00:56:06 +0800

    seggs

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; J.O.J.O</p>
+      </div>
+      <!-- /.container -->
+    </footer>
